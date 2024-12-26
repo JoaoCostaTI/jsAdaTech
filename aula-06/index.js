337d4f6de410b4eccdeb998421efc1e32f473967
JoaoCostaTI <joao.costapp@outlook.com>
2024-12-26 11:16:34 -0300

    Update index.js

--- aula-06/index.js
+++ aula-06/index.js
@@ -1,2 +1,39 @@
-console.log("Hello World")
-console.log("Estamos na aula 06")
+//Tipos de variáveis
+// string
+// int (Number)
+// number
+// boolean
+// unidefined 
+// object
+// function
+
+// JS possui tipagem fraca
+// 
+
+var nomeDoProfessor = "João Costa" 
+var idade = 31
+var altura = 1.76
+var estudando = true
+
+console.log(nomeDoProfessor, typeof nomeDoProfessor)
+console.log(idade, typeof idade)
+console.log(altura, typeof altura)
+console.log(estudando, typeof estudando)
+
+var semConteudo
+
+console.log(semConteudo)
+
+var curso = "frontEnd em React", topico = "JS Basico 1"
+
+console.log(curso, topico)
+
+//let e const permite alterar uma variavel já criada
+
+let notaDoAluno = 10
+const mediaDoAluno = 8
+
+notaDoAluno = 9
+//mediaDoAluno = 7
+
+console.log(notaDoAluno, mediaDoAluno)
