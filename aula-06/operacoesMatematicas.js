7c87fe7b41a96676867006987c051ae9b2c3a531
JoaoCostaTI <joao.costapp@outlook.com>
2024-12-27 08:47:55 -0300

    novoarquivo

--- aula-06/operacoesMatematicas.js
+++ aula-06/operacoesMatematicas.js
@@ -2,5 +2,13 @@ const nota1 = 10
 const nota2 = 8
 
 const somaDasNotas = nota1 + nota2
+let mediaDasNotas = somaDasNotas / 2
 
-console.log('Soma das notas é: ', somaDasNotas)
+mediaDasNotas = (nota1 + nota2) / 2
+
+console.log('Soma das notas é:', somaDasNotas)
+console.log('Media Das Notas é:', mediaDasNotas)
+
+const numero = 16
+
+console.log('Raiz quadrada de', numero, '=', Math.sqrt(numero))
