929858025342e785e92ac0f58c4603916b51a9d4
JoaoCostaTI <joao.costapp@outlook.com>
2024-12-30 16:03:02 -0300

    Update assincronismo.js

--- aula-13/assincronismo.js
+++ aula-13/assincronismo.js
@@ -2,12 +2,25 @@ const fs = require('fs')
 
 // * 1. Call Backs
 
-// Função Assincrona, ler um arquivo leva um tempo
-fs.readFile('aula-13/arquivo.txt', (erro, conteudoDoArquivo) => {
-    if(erro){
-        console.log('Ocorreu um erro ao tentar ler o arquivo', erro)
-    }else{
-        console.log(String(conteudoDoArquivo))
-    }
-})
+// console.log('ANTES de Ler o arquivo')
 
+// // Função Assincrona, ler um arquivo leva um tempo
+// fs.readFile("arquivo.txt", (erro, conteudoDoArquivo) => {
+//     if(erro){
+//         console.log("Ocorreu um erro ao tentar ler o arquivo", erro)
+//     }else{
+//         console.log(String(conteudoDoArquivo))
+//     }
+// })
+
+// console.log('DEPOIS da função de ler o arquivo')
+
+// Exemplo 2 setTomeout (Função que cria um timer)
+
+console.log('ANTES do SetTimeOut')
+
+setTimeout(() => {
+    console.log('Isso sera executado apos 2s')
+}, 2 * 1000);
+
+console.log('DEPOIS do SetTimeOut')
