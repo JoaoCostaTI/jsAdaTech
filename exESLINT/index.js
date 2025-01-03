5da50103204790ac15a1c8403a0a3f2300d698e2
JoaoCostaTI <joao.costapp@outlook.com>
2025-01-03 09:52:27 -0300

    Update index.js

--- exESLINT/index.js
+++ exESLINT/index.js
@@ -1,6 +1,34 @@
-const aula = "JASON"
-const aula2 = 'joao'
+// const aula = "JASON"
+// const aula2 = 'joao'
 
-console.log(aula)
+// console.log(aula)
 
-const joao = "joaasd"
+// const joao = "joaasd"
+
+// const fs = require('fs')
+// const { type } = require('os')
+
+// fs.readFile('.eslintrc.json', (erro, dados) =>{
+//     if (erro){
+//         console.log('Erro: ', erro)
+//     }else{
+//        const dadosObjeto = JSON.parse(dados)
+//         console.log(dadosObjeto.rules)
+//     }
+// })
+
+// const jsonString = '{"nome": "Joao Mendes"}';
+
+// console.log(JSON.parse(jsonString))
+
+// console.log(typeof jsonString)
+
+const pessoa = {
+    nome: "Joao Mendes",
+    papel: 'Programador'
+}
+
+console.log(pessoa)
+console.log(typeof pessoa)
+console.log(JSON.stringify(pessoa))
+console.log(typeof pessoa)
